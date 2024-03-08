b6be7f53973828a7153f6a5574f8c3d03a9cdde3
William dos Santos <willdossantosdesigns@gmail.com>
2024-03-08 10:48:03 -0800

    links

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import { BiBookOpen, BiEnvelope, BiMapPin, BiPhone } from "react-icons/bi";
+import Link from "next/link";
 
 function Footer() {
   return (
@@ -43,7 +44,10 @@ function Footer() {
                 The Woodlands, TX. 77380
               </li>
               <li className="footer-details_border">
-                <a style={{ display: "flex", columnGap: "10px" }} href="mailto:hello@multisiglabs.org?subject=New%20Message%20for%20Multisig%20Labs%20Team">
+                <a
+                  style={{ display: "flex", columnGap: "10px" }}
+                  href="mailto:hello@multisiglabs.org?subject=New%20Message%20for%20Multisig%20Labs%20Team"
+                >
                   <BiEnvelope />
                   hello@multisiglabs.org
                 </a>
@@ -53,7 +57,7 @@ function Footer() {
               </li>
             </ul>
           </li>
-          <li>
+          <li style={{ display: "none" }}>
             <div class="list-header">Legal</div>
             <ul className="footer-details">
               <li>Terms and Conditions</li>
@@ -62,16 +66,30 @@ function Footer() {
           <li>
             <div class="list-header">Products</div>
             <ul className="footer-details">
-              <li>GoGoPool</li>
-              <li>Pandasia</li>
+              <li>
+                <a href="https://www.gogopool.com/">GoGoPool</a>
+              </li>
+              <li>
+                <a href="https://www.pandasia.io/">Pandasia</a>
+              </li>
             </ul>
           </li>
           <li>
             <div class="list-header">Company</div>
             <ul className="footer-details">
-              <li>About</li>
-              <li>Twitter</li>
-              <li>LinkedIn</li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="https://twitter.com/GoGoPool_">
+                Twitter
+                </Link>
+              </li>
+              <li>
+              <Link href="https://www.linkedin.com/company/gogopool">
+                LinkedIn
+                </Link>
+                </li>
             </ul>
           </li>
         </ul>
